6dd339a4a4617206bcdbe13da3f4898db24b6921
Huy Pham <huypham060398@gmail.com>
2025-10-31 00:02:34 +0200

    fix WMO texture selection

--- src/js/3D/exporters/WMOExporter.js
+++ src/js/3D/exporters/WMOExporter.js
@@ -311,7 +311,7 @@ class WMOExporter {
 				for (let i = 0; i < batch.numFaces; i++)
 					indices[i] = group.indices[batch.firstFace + i] + indOfs;
 
-				const matID = batch.flags === 2 ? batch.possibleBox2[2] : batch.materialID;
+				const matID = ((batch.flags & 2) === 2) ? batch.possibleBox2[2] : batch.materialID;
 				gltf.addMesh(groupName + bI, indices, texMaps.materialMap.get(matID));
 			}
 
@@ -463,7 +463,7 @@ class WMOExporter {
 				for (let i = 0; i < batch.numFaces; i++)
 					indices[i] = group.indices[batch.firstFace + i] + indOfs;
 
-				const matID = batch.flags === 2 ? batch.possibleBox2[2] : batch.materialID;
+				const matID = ((batch.flags & 2) === 2) ? batch.possibleBox2[2] : batch.materialID;
 				obj.addMesh(groupName + bI, indices, materialMap.get(matID));
 			}
 
